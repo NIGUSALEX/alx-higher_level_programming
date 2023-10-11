6460b605544a97de28add69731c2f75bfb176d04
NIGUSALEX <aalemnguss@gmail.com>
2023-10-11 05:00:48 -0700

    python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# Author - NIGUSALEX
-
 def islower(c):
-    """Function checks for lowercase characters."""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
         return False
-

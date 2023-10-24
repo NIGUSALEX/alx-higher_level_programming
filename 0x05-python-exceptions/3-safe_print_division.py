e02d91cbc79e861f3460aec815ee8dcc6e979506
NIGUSALEX <aalemnguss@gmail.com>
2023-10-24 02:07:52 -0700

    manadatory final correction

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-
 def safe_print_division(a, b):
-    quotient = None
     try:
-        quotient = a / b
-        print("Inside result: {}".format(quotient))
-    except:
-        print("Inside result: {}".format(quotient))
+        div = a / b
+    except (ZeroDivisionError):
+        div = None
     finally:
-        return quotient
+        print("Inside result: {}".format(div))
+        return div
+

e0d1f47120c6b321652b9ed1d6496b5e2d590e4a
NIGUSALEX <aalemnguss@gmail.com>
2023-10-24 03:00:34 -0700

    added code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-
 
 def magic_calculation(a, b):
-    """A function based on a python bytee code"""
-    return (result)
+    """function that does exactly the same as the given Python bytecode.
+
+    Args:
+        a: integer
+        b: integer
+    """
+    result = 0
+
+    for num in range(1, 3):
+        try:
+            if num > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / num
+        except Exception:
+            result = b + a
+            break
+    return result
